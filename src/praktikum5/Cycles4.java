79e1190c89169215b74bab64dc501427185cd77d
Edgar Tereping <eterepin@klass1-06-l.intra.itcollege.ee>
2016-09-16 11:29:07 +0300

    Praktlopp

--- src/praktikum5/Cycles4.java
+++ src/praktikum5/Cycles4.java
@@ -1,5 +1,22 @@
 package praktikum5;
 
 public class Cycles4 {
+	public static void main(String[] args) {
+		int i, j, tableSize;
+		tableSize = 7;
 
+		for (i = 0; i < tableSize; i++) {
+			for (j = 0; j < tableSize; j++) {
+				if (i == j) {
+					System.out.print("1");
+				} else {
+					System.out.print("0");
+
+				}
+
+			}
+			System.out.println();
+		}
+
+	}
 }
