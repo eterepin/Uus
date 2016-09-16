2694b539d82450653fd7d687fe685b871204be32
EdgarTereping <edgar.tereping@gmail.com>
2016-09-16 11:52:21 +0300

    Test commit 2

--- src/praktikum5/Cycles4.java
+++ src/praktikum5/Cycles4.java
@@ -4,7 +4,6 @@ public class Cycles4 {
 	public static void main(String[] args) {
 		int i, j, tableSize;
 		tableSize = 7;
-
 		for (i = 0; i < tableSize; i++) {
 			for (j = 0; j < tableSize; j++) {
 				if (i == j) {
